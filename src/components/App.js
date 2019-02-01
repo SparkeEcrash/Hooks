fdf03c514a87d848d5d92e692eb201c5e4889b08
SparkeEcrash <jpark121989@gmail.com>
2019-01-31 22:25:21 -0800

    finished initial-setup

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+
+const App = () => {
+  //useState('posts') returns an array of two items
+  const [resource, setResource] = useState('posts');
+  //const resource gets assigned to the first item and is the value of state
+  //const setResource gets assigned to the second item and is a function that sets the value of state
+
+  //'posts' sets the initial value of the state
+
+  const [currentCount, setCount] = useState(0);
+
+
+    return (
+      <div>
+        <div>
+            <button onClick={() => setResource('posts')}>Posts</button>
+            <button onClick={() => setResource('todos')}>Todos</button>
+        </div>
+        <ResourceList resource={resource}/>
+      </div>
+    );
+}
+
+export default App;
